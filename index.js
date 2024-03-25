134c558fcd2c7f8a32b52a406c03a91895d0ab52
jeremymiru <mirujeremy@gmail.com>
2024-03-25 22:25:08 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function receivesAFunction(callback) {
+    callback() ;
+}
+
+function returnsANamedFunction() {
+    function namedFunction() {
+
+    }
+    return namedFunction ;
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    }
+}
